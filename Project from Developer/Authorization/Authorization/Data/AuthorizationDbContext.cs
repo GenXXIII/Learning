ee157f3ae356eee2900c244e1c75819f889f9d8c
GenXXIII <yimbunleap@gmail.com>
2025-06-04 14:49:59 +0700

    Add files via upload

---
+++ Project from Developer/Authorization/Authorization/Data/AuthorizationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authorization.Data
+{
+    public class AuthorizationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthorizationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
